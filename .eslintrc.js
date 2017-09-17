b4afbffac5c6da48a69022092150bfb77e57b26a
William Shiao <willshiao@gmail.com>
2017-09-17 00:04:58 -0700

    Set up environment

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    mocha: true
+  },
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'script',
+    "impliedStrict": false
+  },
+  rules: {
+    // 'no-console': 0,
+    'consistent-return': 0,
+    'keyword-spacing': ['error', {'overrides': {
+      'if': {'after': false},
+      'for': {'after': false},
+      'while': {'after': false}
+      'catch': {'after': false}
+    }}],
+    'no-underscore-dangle': [ 'error', { 'allow': [ '_id' ] } ],
+    'linebreak-style': 'off',
+    'no-param-reassign': ['error', { 'props': false }],
+  }
+};
