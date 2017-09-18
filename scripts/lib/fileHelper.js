4d8a5614717a4b9ee9632cbdb2560794332daec3
William Shiao <willshiao@gmail.com>
2017-09-18 00:49:39 -0700

    Add data for all courses

--- scripts/lib/fileHelper.js
+++ scripts/lib/fileHelper.js
@@ -7,13 +7,13 @@ const fs = Promise.promisifyAll(require('fs'));
 
 module.exports = {
   getCatalogPath() {
-    const subjects = config.get('catalog.subjects').split(',').join('-');
+    const subjects = config.get('catalog.subjects').split(',').join('-') || 'all';
     const filename = `${config.get('catalog.term')}_${subjects}_catalog.json`;
     return path.join(config.get('dataDir'), filename);
   },
 
   getPrereqCatalogPath() {
-    const subjects = config.get('catalog.subjects').split(',').join('-');
+    const subjects = config.get('catalog.subjects').split(',').join('-') || 'all';
     const filename = `${config.get('catalog.term')}_${subjects}_prereqs.json`;
     return path.join(config.get('dataDir'), filename);
   },
