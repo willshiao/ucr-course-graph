9114812e4b084587f3865e3ef9bcb7ba868199c1
William Shiao <willshiao@gmail.com>
2017-09-19 00:18:28 -0700

    Modify graph script for use with protovis

--- scripts/graph.js
+++ scripts/graph.js
@@ -9,41 +9,45 @@ const fileHelper = require('./lib/fileHelper');
 
 function createNode(data, subjects) {
   return {
-    id: data.subjectCourse,
+    nodeName: data.subjectCourse,
     fullName: data.courseTitle,
     group: _.findIndex(subjects, sub => sub.code === data.subject),
   };
 }
 
-function createEdge(course, data, type) {
-  console.log('Created edge:', course.subjectCourse, '->', data.course);
+function createEdge(locMap, source, data, type) {
+  console.log('Created edge:', source, '->', data.course);
+  if(!(data.course in locMap)) {
+    console.log('Skipped edge');
+    return null;
+  }
   return {
-    source: course.subjectCourse,
-    target: data.course,
+    source: locMap[source],
+    target: locMap[data.course],
     type,
     allowConcurrent: !data.disallowConcurrent,
   };
 }
 
 function createGraph(catalog, subjects) {
-  const freqMap = {};
+  const locMap = {};
   const output = {
     nodes: [],
     edges: [],
   };
 
-  _.forEach(catalog, (course) => {
-    freqMap[course.subjectCourse] = true;
+  _.forEach(catalog, (course, index) => {
     output.nodes.push(createNode(course, subjects));
+    locMap[course.subjectCourse] = index;
     if(!course.prereqs || !course.prereqs.and) return;
 
     output.edges = output.edges
-      .concat(course.prereqs.and.map(req => createEdge(course, req, 'and')))
-      .concat(course.prereqs.or.map(req => createEdge(course, req, 'or')));
+      .concat(course.prereqs.and.map(req => createEdge(locMap, course.subjectCourse, req, 'and')))
+      .concat(course.prereqs.or.map(req => createEdge(locMap, course.subjectCourse, req, 'or')));
   });
 
   console.log('Edges initially:', output.edges.length);
-  output.edges = output.edges.filter(edge => edge.target in freqMap);
+  output.edges = output.edges.filter(edge => edge);
   console.log('Edges after filtering:', output.edges.length);
   return output;
 }
